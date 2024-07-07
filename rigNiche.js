f88d137a0a57161fbffebbd40db980e816b7e258
rohanmandal303 <163116277+rohanmandal303@users.noreply.github.com>
2024-07-07 11:56:59 +0530

    Update rigNiche.js

--- rigNiche.js
+++ rigNiche.js
@@ -1,16 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('nav ul li a');
-    
-    navLinks.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            const targetId = link.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
-            
-            window.scrollTo({
-                top: targetSection.offsetTop - 50,
-                behavior: 'smooth'
-            });
-        });
-    });
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formm = event.target;
+  const name = formm.elements.name.value;
+  const email = formm.elements.email.value;
+  const message = formm.elements.message.value;
+
+  console.log(`Name: ${name}`);
+  console.log(`Email: ${email}`);
+  console.log(`Message: ${message}`);
 });
